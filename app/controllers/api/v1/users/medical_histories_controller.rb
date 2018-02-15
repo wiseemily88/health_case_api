bbf7aa5ff84aaecf5fe2faf6fa1233e7e4c500ff
Emily Wise <wise.m.emily@gmail.com>
2018-02-14 19:50:56 -0700

    POST api/v1/users/:id/medical_history endpoint complete

--- app/controllers/api/v1/users/medical_histories_controller.rb
+++ app/controllers/api/v1/users/medical_histories_controller.rb
@@ -1,17 +1,27 @@
 class Api::V1::Users::MedicalHistoriesController < ApplicationController
 before_action :find_user
+before_action :find_medical_history_item, only: :create
 
   def index
     render json: @user.medical_histories
   end
 
-
-
-
+  def create
+    if @user && @medicalhistory
+      @user.medical_histories << @medicalhistory
+      render json:{ message: "Successfully added #{@medicalhistory.name} to #{@user.email}"}, status: 201
+   else
+      render json: { message: "Cannot find requested medicalhistory and/or user" }, status: 404
+    end
+  end
 
 private
 
   def find_user
     @user ||= User.find_by(id: params[:user_id])
+  end
+
+  def find_medical_history_item
+    @medicalhistory ||= MedicalHistory.find_by(id: params[:medical_history_id])
   end
 end
--- spec/api/v1/users/user_medical_histories_spec.rb
+++ spec/api/v1/users/user_medical_histories_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'user medical histories API' do
 
  let!(:user) {create(:user_with_medical_history)}
+  let!(:medical_history) {create(:medical_history)}
 
 
   describe "#index" do
@@ -12,7 +13,16 @@ RSpec.describe 'user medical histories API' do
       history = JSON.parse(response.body)
 
       expect(response.status).to eq(200)
-      # expect(history.first["name"]).to eq(user.medical_histories.first.name)
+      expect(history.first["name"]).to eq(user.medical_histories.first.name)
+    end
+  end
+
+  describe "#create" do
+    it "it can create a new user medical history" do
+      post "/api/v1/users/#{user.id}/medical_histories/#{medical_history.id}"
+
+    expect(response.status).to eq(201)
+    expect(user.medical_histories).to include(medical_history)
     end
   end
 end
