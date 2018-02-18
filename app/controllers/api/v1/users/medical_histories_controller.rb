081434db1bf0a272ee859c7bb54240385157afee
Emily Wise <wise.m.emily@gmail.com>
2018-02-17 18:28:10 -0700

    added comment

--- app/controllers/api/v1/users/medical_histories_controller.rb
+++ app/controllers/api/v1/users/medical_histories_controller.rb
@@ -7,6 +7,7 @@ before_action :find_medical_history_item, only: [:create, :destroy, :update]
   end
 
   def create
+# test is passing but keep getting a 422
     if @user && @medicalhistory
       @user.medical_histories << @medicalhistory
       render json:{ message: "Successfully added #{@medicalhistory.name} to #{@user.email}"}, status: 201
