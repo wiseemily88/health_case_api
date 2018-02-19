0c09edc77bcee8f4fdf31fac72c3fc964384274a
Emily Wise <wise.m.emily@gmail.com>
2018-02-19 09:44:07 -0700

    complete User Surgical history endpoints

--- spec/api/v1/users/user_surgical_histories_spec.rb
+++ spec/api/v1/users/user_surgical_histories_spec.rb
@@ -18,58 +18,58 @@ RSpec.describe 'user surgical histories API' do
     end
   end
 
-#   describe "#create" do
-#     context "the user and medical history already exist" do
-#       it "it can create a new user medical history" do
-#         post "/api/v1/users/#{user.id}/medical_histories/#{medical_history.id}"
-#
-#         expect(response.status).to eq(201)
-#         expect(user.medical_histories).to include(medical_history)
-#       end
-#     end
-#
-#     context "the user or medical history does not exist" do
-#       it "it can create a new user medical history" do
-#         post "/api/v1/users/50/medical_histories/7"
-#
-#         expect(response.status).to eq(404)
-#       end
-#     end
-#   end
-#
-#   describe "#delete" do
-#     context "the user and medical history already exist" do
-#
-#       it "it can delete a user's medical history" do
-#         @medicalhistory_2 = create(:medical_history)
-#         user.medical_histories << @medicalhistory_2
-#         delete "/api/v1/users/#{user.id}/medical_histories/#{@medicalhistory_2.id}"
-#
-#         user.reload
-#         expect(response.status).to eq(200)
-#         expect(user.medical_histories).to_not include(@medicalhistory_2)
-#       end
-#     end
-#   end
-#   context "the user or medical history does not exist" do
-#     it "it can create a new user medical history" do
-#       delete "/api/v1/users/5/medical_histories/7"
-#
-#       expect(response.status).to eq(404)
-#     end
-#   end
-#
-# describe "#update" do
-#
-#     it "it can update a user's medical history" do
-#       @medicalhistory_2 = create(:medical_history)
-#       user.medical_histories << @medicalhistory_2
-#
-#       patch "/api/v1/users/#{user.id}/medical_histories/#{@medicalhistory_2.id}", params: {:note => "I had this condition during the summer of 2009"}
-#       user_medical_history = UserMedicalHistory.find_by(user_id: user.id, medical_history_id: @medicalhistory_2.id)
-#
-#       expect(response).to be_success
-#       expect(user_medical_history.note).to eq("I had this condition during the summer of 2009")
-#     end
-#   end
+  describe "#create" do
+    context "the user and surgical history already exist" do
+      it "it can create a new user surgical history" do
+        post "/api/v1/users/#{user.id}/surgical_histories/#{surgical_history.id}"
+
+        expect(response.status).to eq(201)
+        expect(user.surgical_histories).to include(surgical_history)
+      end
+    end
+
+    context "the user or surgical history does not exist" do
+      it "it cannot create a new user surgical history" do
+        post "/api/v1/users/50/medical_histories/70"
+
+        expect(response.status).to eq(404)
+      end
+    end
+  end
+
+  describe "#delete" do
+    context "the user and surgical history already exist" do
+
+      it "it can delete a user's surgical history" do
+        @surgicalhistory_2 = create(:surgical_history)
+        user.surgical_histories << @surgicalhistory_2
+        delete "/api/v1/users/#{user.id}/surgical_histories/#{@surgicalhistory_2.id}"
+
+        user.reload
+        expect(response.status).to eq(200)
+        expect(user.surgical_histories).to_not include(@surgicalhistory_2)
+      end
+    end
+  end
+  context "the user or surgical history does not exist" do
+    it "it cannot create a new user surgical history" do
+      delete "/api/v1/users/5/surgical_histories/70"
+
+      expect(response.status).to eq(404)
+    end
+  end
+
+describe "#update" do
+
+    it "it can update a user's surgical history" do
+      @surgicalhistory_2 = create(:surgical_history)
+      user.surgical_histories << @surgicalhistory_2
+
+      patch "/api/v1/users/#{user.id}/surgical_histories/#{@surgicalhistory_2.id}", params: {:note => "I had this condition during the summer of 2009"}
+      user_surgical_history = UserSurgicalHistory.find_by(user_id: user.id, surgical_history_id: @surgicalhistory_2.id)
+
+      expect(response).to be_success
+      expect(user_surgical_history.note).to eq("I had this condition during the summer of 2009")
+    end
+  end
 end
